42a3d7570da14df90f11ed81e5817d08685e1763
QuackDuster <jjimenez3@ucol.mx>
2022-03-17 20:05:57 -0600

    Updating gestures and adding zoom functionality
    Adding the functionality of pinch to zoom to the images

--- GesturesPractice/Modal2.xaml.cs
+++ GesturesPractice/Modal2.xaml.cs
@@ -20,8 +20,52 @@ namespace GesturesPractice
 
         void OnSwipeGestureRecognizerSwiped(object sender, EventArgs args)
         {
-              
+
             this.Navigation.PopModalAsync();
         }
+
+        private double startScale;
+        private double currentScale;
+        private double xOffset;
+        private double yOffset;
+
+        void OnPinchZoom(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+                startScale = Content.Scale;
+                Content.AnchorX = 0;
+                Content.AnchorY = 0;
+            }
+
+            if (e.Status == GestureStatus.Running)
+            {
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+                double renderedX = Content.X + xOffset;
+                double deltaX = renderedX / Width;
+                double deltaWidth = Width / (Content.Width * startScale);
+                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+                double renderedY = Content.Y + yOffset;
+                double deltaY = renderedY / Height;
+                double deltaHeight = Height / (Content.Height * startScale);
+                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+                Content.TranslationX = Math.Min(0, Math.Max(targetX, -Content.Width * (currentScale - 1)));
+                Content.TranslationY = Math.Min(0, Math.Max(targetY, -Content.Height * (currentScale - 1)));
+            }
+
+            Content.Scale = currentScale;
+            {
+                if (e.Status == GestureStatus.Completed)
+                {
+                    xOffset = Content.TranslationX;
+                    yOffset = Content.TranslationY;
+                }
+            }
+
+        }
+
     }
 }
